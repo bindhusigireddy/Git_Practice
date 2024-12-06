f66c30251d4d41e2989c197c06d44130d0956c60
yesh198 <yeshwanthv018@gmail.com>
2024-12-06 13:32:10 +0530

    [002] -This is modified code

--- Initial.py
+++ Initial.py
@@ -1,5 +1,12 @@
+
+##Sorting Logic 
 a=[2,9,51,6]
 a.sort()
 b = sorted(a)
 print(a, "Using sort")
 print(b, "Using sorted")
+
+##Mapping logic
+l = [1,2,3,4,5]
+output_list = list(map(lambda x: x+1,l))
+print(output_list," Mapped List")
